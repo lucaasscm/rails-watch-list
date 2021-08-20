6b526a4e87b864f243478296570ded6e0c47538b
Lucas Martelli <lucas98martelli@hotmail.com>
2021-08-19 21:48:52 -0300

    front-end init

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'bookmarks/create'
+  get 'bookmarks/new'
+  get 'bookmarks/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, except: [:edit, :update, :destroy] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
