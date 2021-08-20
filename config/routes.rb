a646cb59eb251582bb70551e2d9fd84a8537fc68
Lucas Martelli <lucas98martelli@hotmail.com>
2021-08-20 12:22:27 -0300

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'bookmarks/create'
-  get 'bookmarks/new'
-  get 'bookmarks/destroy'
+  root to: 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, except: [:edit, :update, :destroy] do
+  resources :lists, except: [:edit, :update, :destroy, :index] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
